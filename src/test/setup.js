4be58a2e50e364414cc6949f889bacadfedf78e7
Vira Murray <vira.avramenko@gmail.com>
2023-06-29 16:04:03 -0400

    added issues workflow

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,1 +1,3 @@
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+//how to add sourcetype module to jest
+//https://stackoverflow.com/questions/54151871/jest-typescript-jest-does-not-know-how-to-handle-module-aliases
